835f569d86154bfe07dbce97dfe846296db28760
Ravina <ravinakaur@hotmail.com>
2018-02-20 21:14:18 -0800

    Now using mySQL

--- todolist/settings.py
+++ todolist/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todolist',
+        'USER': 'root',
+        'PASSWORD': 'admin123',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    #!!!created_at = models.DateTimeField(default=datetime.now, blank=True)
