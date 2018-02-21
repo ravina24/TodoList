5aa2ba3d946618a0c37a70fc7781bc6cb1abe38c
Ravina <ravinakaur@hotmail.com>
2018-02-21 10:56:36 -0800

    CSS now working!

--- todos/urls.py
+++ todos/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from . import views # . is current folder
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^details/(?P<id>\w{0,50})/$', views.details),
     url(r'^add', views.add, name='add'),
 ]
+
+urlpatterns += staticfiles_urlpatterns() # to load css
