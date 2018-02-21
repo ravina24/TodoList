963df54c2ad02cbb2cd59c62077aa1b8371094a4
Ravina <ravinakaur@hotmail.com>
2018-02-20 21:43:17 -0800

    Todos working on backend. Implement frontend

--- todos/models.py
+++ todos/models.py
@@ -4,5 +4,7 @@ from datetime import datetime
 class Todo(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    #!!!created_at = models.DateTimeField(default=datetime.now, blank=True)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title # todolist object will now show as title instead of default name
